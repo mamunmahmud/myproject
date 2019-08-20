41ebcdedf098f9dfe963bcbe6dab592997482011
mamunmahmud <mamun700@gmail.com>
2019-08-19 22:50:26 -0400

    upload file

---
+++ eclipse-workspace/Inheritance/src/polymorphism/ModernLandCalculator.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class ModernLandCalculator extends LandCalculator {
+
+	@Override
+	public int areaOfLand(int a, int b, int c) {
+		int total= a+b+c-1;
+		return total;
+	}
+
+	
+
+}
