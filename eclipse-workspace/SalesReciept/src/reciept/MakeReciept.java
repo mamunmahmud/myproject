9b5dcc2beb3088bf20ca608fcee0b43288af6ced
mamunmahmud <mamun700@gmail.com>
2019-09-08 23:52:08 -0400

    add code

---
+++ eclipse-workspace/SalesReciept/src/reciept/MakeReciept.java
@@ -0,0 +1,40 @@
+package reciept;
+
+import java.util.Scanner;
+
+public class MakeReciept {
+
+
+		
+     public static String name;
+     public static int  quanity;
+     public static int orice;
+	
+	
+	public static void getInput() {
+		
+	 System.out.println("======Sales Reciept=====");	
+	// System.out.println("Please enter name");	
+	
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Please ente namer");	
+	name=sc.nextLine();
+	
+	
+	
+	}
+	
+	
+
+
+
+
+public static void main(String[] args) {
+
+
+getInput();
+
+
+}
+
+}
