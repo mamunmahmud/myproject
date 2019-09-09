b4956ffefc34ccb9be7acbb080cfbd21789e7991
mamunmahmud <mamun700@gmail.com>
2019-09-09 01:06:14 -0400

    jj

---
+++ eclipse-workspace/Selenium2019/src/eightClass/UseCsanner.java
@@ -0,0 +1,16 @@
+package eightClass;
+
+import java.util.Scanner;
+
+public class UseCsanner {
+  
+	public static void main(String[] args) {
+		System.out.println("Please enter your value");
+		Scanner sc=new Scanner(System.in);
+       int num1=sc.nextInt();
+	   int num2=sc.nextInt();
+	   System.out.println(num1);
+	   System.out.println(num2);
+	}
+
+}
