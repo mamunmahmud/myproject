be631841119302901621e0d341888913d74ec8ac
mamunmahmud <mamun700@gmail.com>
2019-09-09 01:08:52 -0400

    ggg

---
+++ eclipse-workspace/Selenium2019/src/eightClass/TestSingleton.java
@@ -0,0 +1,15 @@
+package eightClass;
+
+public class TestSingleton {
+
+	public static void main(String[] args) {
+	
+SingletonClass singleton= SingletonClass.getInstance();
+System.out.println(singleton.password);
+	
+//SingletonClass singletonClass=SingletonClass();
+	
+	
+	}
+
+}
