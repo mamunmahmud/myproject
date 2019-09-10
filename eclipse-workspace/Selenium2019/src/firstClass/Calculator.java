8b64e10d9332b79d83de9e8a42c7a874108ee1df
mamunmahmud <mamun700@gmail.com>
2019-09-09 20:58:29 -0400

    class1

---
+++ eclipse-workspace/Selenium2019/src/firstClass/Calculator.java
@@ -0,0 +1,26 @@
+package firstClass;
+
+public class Calculator {
+
+
+	public int addition(int num1, int num2) {
+	
+	
+	
+	int total=num1+num2;
+System.out.println(total);
+  return total;
+
+}
+public void additionDisplay(int num1, int num2) {
+	
+	int total=num1+num2;
+	System.out.println(total);
+	
+}
+
+
+
+
+
+}
