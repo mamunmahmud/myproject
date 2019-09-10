82935f23165ea0c3ae64f21c977647aadfa47175
mamunmahmud <mamun700@gmail.com>
2019-09-09 21:08:40 -0400

    jjjj

---
+++ eclipse-workspace/Selenium2019/src/firstClass/TestCalculator.java
@@ -0,0 +1,15 @@
+package firstClass;
+
+public class TestCalculator {
+
+	public static void main(String[] args) {
+		Calculator cal= new Calculator();
+
+	 cal.addition(10, 20);
+	
+	
+	cal.additionDisplay(20, 30);
+	
+	}
+
+}
