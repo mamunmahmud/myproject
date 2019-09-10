17b5053ce97d70303540095298742932661ae147
mamunmahmud <mamun700@gmail.com>
2019-09-09 21:00:48 -0400

    uuu

---
+++ eclipse-workspace/Selenium2019/src/firstClass/House.java
@@ -0,0 +1,17 @@
+package firstClass;
+
+public class House {
+
+int address=31;
+String houseName="PeopleNtech";
+
+public void classRoom() {
+	System.out.println("We teach java in our class");
+
+}
+public void adminRoom() {
+	
+	System.out.println("Admin room is for admin");
+}
+
+}
